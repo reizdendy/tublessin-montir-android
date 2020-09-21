d52a970f526bec2d2d12f5d586141d1a44ac4a13
reizdendy <36389535+reizdendy@users.noreply.github.com>
2020-09-21 23:48:07 +0700

    fix chat by  montir and/or customer

--- app/src/main/java/com/example/tublessin_montir/domain/chat/recycleview/ChatRecycleViewAdapter.kt
+++ app/src/main/java/com/example/tublessin_montir/domain/chat/recycleview/ChatRecycleViewAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.tublessin_montir.domain.chat.recycleview
 
 import android.R
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tublessin_montir.domain.chat.Conversation
+import com.pixplicity.easyprefs.library.Prefs
 
 
 class ChatRecycleViewAdapter(
@@ -25,14 +29,26 @@ class ChatRecycleViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        holder.message.text = messageList[position].message
+        val montirId = Prefs.getString("id", "0")
+        if (messageList[position].sender_id == montirId){
+            holder.messageMontir.isVisible = true
+            holder.messageMontir.text = messageList[position].message
+            holder.imageMontir.isVisible = true
+        } else {
+            holder.messageCustomer.isVisible = true
+            holder.messageCustomer.text = messageList[position].message
+            holder.imageCustomer.isVisible = true
+        }
         holder.senderId.text = messageList[position].sender_id
         holder.date.text = messageList[position].date_created
     }
 }
 
 class ChatViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val message = view.findViewById<TextView>(com.example.tublessin_montir.R.id.text_message_body)
+    val imageMontir = view.findViewById<ImageView>(com.example.tublessin_montir.R.id.image_message_profile)
+    val imageCustomer = view.findViewById<ImageView>(com.example.tublessin_montir.R.id.image_message_profile2)
+    val messageMontir = view.findViewById<TextView>(com.example.tublessin_montir.R.id.text_message_body)
+    val messageCustomer = view.findViewById<TextView>(com.example.tublessin_montir.R.id.text_message_body2)
     val senderId = view.findViewById<TextView>(com.example.tublessin_montir.R.id.text_message_name)
     val date = view.findViewById<TextView>(com.example.tublessin_montir.R.id.text_message_time)
 }
